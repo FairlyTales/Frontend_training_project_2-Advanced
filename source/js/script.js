3ce8e9424dd519f5f0483a343746bc4f70cc16b2
Zura <khnychkin_kirill@tutanota.com>
2020-08-25 02:06:35 +0300

    Finished styling of the mobile version; echanced the JS function which shows/hides & enables/disables the features depending on the screen size; created a couple of new BEM blocks for footer; learned how to properly crop & center images; TODO: basic styling of the tablet & desktop versions, lecture_5, finish styling of the navigation bar for all 3 versions, style hover, active, ect. for all versions.

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,25 @@
+//* --------------Variables --------------------
 // variables for checking the user's screen width
 var tabletWidth = window.matchMedia('(min-width: 768px)')
-var desktopWidth = window.matchMedia('(min-width: 1200x)')
+var desktopWidth = window.matchMedia('(min-width: 1200px)')
 
 // variables for hiding/showing elements depending on the user's screen width
 var smallTextTop = document.querySelector('.stats__legend--top')
 var smallTextBottom = document.querySelector('.stats__legend--bottom')
 var writeReviewButton = document.querySelector('.reviews__write')
+var prevReviewButton = document.querySelector('.reviews__prev')
+var nextReviewButton = document.querySelector('.reviews__next')
+var contactsPhoneMobile = document.querySelector('.contacts__phone-mobile')
+var socialTitle = document.querySelector('.social__title')
 
 // variables for slider toggles; block: advantages, news; mobile version;
 var slide = document.querySelectorAll('.slider__item')
-var sliderButton1 = document.querySelector('#slide-1')
-var sliderButton2 = document.querySelector('#slide-2')
-var sliderButton3 = document.querySelector('#slide-3')
-var sliderButton4 = document.querySelector('#slide-4')
-var sliderButton5 = document.querySelector('#slide-5')
-var sliderButton6 = document.querySelector('#slide-6')
+var sliderButtonAdvantages1 = document.querySelector('#advantages-slide-1')
+var sliderButtonAdvantages2 = document.querySelector('#advantages-slide-2')
+var sliderButtonAdvantages3 = document.querySelector('#advantages-slide-3')
+var sliderButtonReviews1 = document.querySelector('#reviews-slide-1')
+var sliderButtonReviews2 = document.querySelector('#reviews-slide-2')
+var sliderButtonReviews3 = document.querySelector('#reviews-slide-3')
 
 // variables for button "show all news"; block: news; mobile version
 var newsToggleStatus = 0
@@ -24,34 +29,50 @@ var newsButton = document.querySelector('.news__button-to-all')
 //* -------------------------------------------
 
 // enables/disables mobile/tablet/desktop features depending on the width of the user's screen
-function checkDeviceWidth(width) {
-  // if device if larger than 768px wide enable tablet & desktop version features
-  if (width.matches) {
-    console.log('tablet/desktop version')
+function checkDeviceWidth() {
+  // if screen is wider than 768px: enable tablet version features
+  if (tabletWidth.matches) {
+    // TODO: remove console.logs after script is finished
+    console.log('tablet version')
 
-    // change position of the .stats_legend
+    // change position of the .stats_legend element
     smallTextTop.classList.remove('visually-hidden')
     smallTextBottom.classList.add('visually-hidden')
+
+    // add "tablet elements" which are hidden in the mobile version
     writeReviewButton.classList.remove('visually-hidden')
+    prevReviewButton.classList.remove('visually-hidden')
+    nextReviewButton.classList.remove('visually-hidden')
+    contactsPhoneMobile.classList.remove('visually-hidden')
   }
-  // if device is smaller than 768px wide enable mobile version featured
+  // if screen is wider than 1200px: also enable desktop version features
+  if (desktopWidth.matches) {
+    // TODO: remove console.logs after script is finished
+    console.log('desktop version')
+
+    // add "desktop elements" which are hidden in the mobile & tablet versions
+    socialTitle.classList.remove('visually-hidden')
+  }
+  // if screen is smaller than 768px: enable only mobile version features
   else {
+    // TODO: remove console.logs after script is finished
     console.log('mobile version')
 
+    // TODO: maybe make the slides change after click on the whole element?
     // slider toggles; block: advantages;
-    sliderButton1.addEventListener('change', function (evt) {
+    sliderButtonAdvantages1.addEventListener('change', function (evt) {
       slide[0].classList.remove('visually-hidden')
       slide[1].classList.add('visually-hidden')
       slide[2].classList.add('visually-hidden')
     })
 
-    sliderButton2.addEventListener('change', function (evt) {
+    sliderButtonAdvantages2.addEventListener('change', function (evt) {
       slide[0].classList.add('visually-hidden')
       slide[1].classList.remove('visually-hidden')
       slide[2].classList.add('visually-hidden')
     })
 
-    sliderButton3.addEventListener('change', function (evt) {
+    sliderButtonAdvantages3.addEventListener('change', function (evt) {
       slide[0].classList.add('visually-hidden')
       slide[1].classList.add('visually-hidden')
       slide[2].classList.remove('visually-hidden')
@@ -59,23 +80,21 @@ function checkDeviceWidth(width) {
 
     // button "show all news"; block: news;
     newsButton.addEventListener('click', function (evt) {
-      // if the news is colapsed, expand them
+      // if the news is colapsed(default): expand them
       if (newsToggleStatus == 0) {
         evt.preventDefault()
-
-        // show 3 more news articles and underlines under them
+        // show 3 more news articles and underlines (pseudo-elements) under them
         for (let i = 2; (i < newsItems.length) & (i != 5); i++) {
           newsItems[i].classList.remove('visually-hidden')
-
-          // i - 1 because by default we have 2 news and 1 underline between them
+          // i - 1 because by default we show 2 news and 1 underline between them
           newsItems[i - 1].classList.add('news__item--with-underline')
         }
+        // rename the button
         newsButton.textContent = 'Скрыть'
-
-        // change the toggle variable
+        // change the toggle-status variable
         newsToggleStatus = 1
       } else {
-        // if newsToggleStatus != 0 (news are expanded, colapse them)
+        // if newsToggleStatus != 0 (news are expanded: colapse them)
         evt.preventDefault()
 
         // colapse 3 news articles & underlines
@@ -86,34 +105,30 @@ function checkDeviceWidth(width) {
         newsButton.textContent = 'Показать все'
         // scroll the screen to the position with the "show all news" button at the top
         newsButton.scrollIntoView()
+        // change the toggle-status variable
         newsToggleStatus = 0
       }
     })
 
-    newsButton.addEventListener('click', function (evt) {})
-
     // slider toggles; block: reviews;
-    sliderButton4.addEventListener('change', function (evt) {
+    sliderButtonReviews1.addEventListener('change', function (evt) {
       slide[3].classList.remove('visually-hidden')
       slide[4].classList.add('visually-hidden')
       slide[5].classList.add('visually-hidden')
     })
 
-    sliderButton5.addEventListener('change', function (evt) {
+    sliderButtonReviews2.addEventListener('change', function (evt) {
       slide[3].classList.add('visually-hidden')
       slide[4].classList.remove('visually-hidden')
       slide[5].classList.add('visually-hidden')
     })
 
-    sliderButton6.addEventListener('change', function (evt) {
+    sliderButtonReviews3.addEventListener('change', function (evt) {
       slide[3].classList.add('visually-hidden')
       slide[4].classList.add('visually-hidden')
       slide[5].classList.remove('visually-hidden')
     })
-    // ---------------------------------------------
-
-    //* button "show all news"; block: news
   }
 }
 
-checkDeviceWidth(tabletWidth)
+checkDeviceWidth()
