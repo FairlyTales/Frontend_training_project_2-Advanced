0dd8e8d1cb2674a6eea3f976ce7805c4fc5ac323
Zura <khnychkin_kirill@tutanota.com>
2020-08-25 21:28:24 +0300

    Added hover/focus/active status to all buttons, tomorrowwill start styling tablet and desktop version, after that will move on to lecture 5

--- source/js/script.js
+++ source/js/script.js
@@ -29,105 +29,117 @@ var newsButton = document.querySelector('.news__button-to-all')
 //* -------------------------------------------
 
 // enables/disables mobile/tablet/desktop features depending on the width of the user's screen
+
+function activateMobileFeatures() {
+  // TODO: remove console.logs after script is finished
+  console.log('mobile version')
+
+  // TODO: maybe make the slides change after click/touch on the whole element?
+  // slider toggles; block: advantages;
+  sliderButtonAdvantages1.addEventListener('change', function (evt) {
+    slide[0].classList.remove('visually-hidden')
+    slide[1].classList.add('visually-hidden')
+    slide[2].classList.add('visually-hidden')
+  })
+
+  sliderButtonAdvantages2.addEventListener('change', function (evt) {
+    slide[0].classList.add('visually-hidden')
+    slide[1].classList.remove('visually-hidden')
+    slide[2].classList.add('visually-hidden')
+  })
+
+  sliderButtonAdvantages3.addEventListener('change', function (evt) {
+    slide[0].classList.add('visually-hidden')
+    slide[1].classList.add('visually-hidden')
+    slide[2].classList.remove('visually-hidden')
+  })
+
+  // button "show all news"; block: news;
+  newsButton.addEventListener('click', function (evt) {
+    // if the news is colapsed(default): expand them
+    if (newsToggleStatus == 0) {
+      evt.preventDefault()
+      // show 3 more news articles and underlines (pseudo-elements) under them
+      for (let i = 2; (i < newsItems.length) & (i != 5); i++) {
+        newsItems[i].classList.remove('visually-hidden')
+        // i - 1 because by default we show 2 news and 1 underline between them
+        newsItems[i - 1].classList.add('news__item--with-underline')
+      }
+      // rename the button
+      newsButton.textContent = 'Скрыть'
+      // change the toggle-status variable
+      newsToggleStatus = 1
+    } else {
+      // if newsToggleStatus != 0 (news are expanded: colapse them)
+      evt.preventDefault()
+
+      // colapse 3 news articles & underlines
+      for (let i = 2; (i < newsItems.length) & (i != 5); i++) {
+        newsItems[i].classList.add('visually-hidden')
+        newsItems[i - 1].classList.remove('news__item--with-underline')
+      }
+      newsButton.textContent = 'Показать все'
+      // scroll the screen to the position with the "show all news" button at the top
+      newsButton.scrollIntoView()
+      // change the toggle-status variable
+      newsToggleStatus = 0
+    }
+  })
+
+  // slider toggles; block: reviews;
+  sliderButtonReviews1.addEventListener('change', function (evt) {
+    slide[3].classList.remove('visually-hidden')
+    slide[4].classList.add('visually-hidden')
+    slide[5].classList.add('visually-hidden')
+  })
+
+  sliderButtonReviews2.addEventListener('change', function (evt) {
+    slide[3].classList.add('visually-hidden')
+    slide[4].classList.remove('visually-hidden')
+    slide[5].classList.add('visually-hidden')
+  })
+
+  sliderButtonReviews3.addEventListener('change', function (evt) {
+    slide[3].classList.add('visually-hidden')
+    slide[4].classList.add('visually-hidden')
+    slide[5].classList.remove('visually-hidden')
+  })
+}
+
+function activateTabletFeatures() {
+  // TODO: remove console.logs after script is finished
+  console.log('tablet version')
+
+  // change position of the .stats_legend element
+  smallTextTop.classList.remove('visually-hidden')
+  smallTextBottom.classList.add('visually-hidden')
+
+  // add "tablet elements" which are hidden in the mobile version
+  writeReviewButton.classList.remove('visually-hidden')
+  prevReviewButton.classList.remove('visually-hidden')
+  nextReviewButton.classList.remove('visually-hidden')
+  contactsPhoneMobile.classList.remove('visually-hidden')
+}
+
+function activateDesktopFeatures() {
+  // TODO: remove console.logs after script is finished
+  console.log('desktop version')
+
+  // add "desktop elements" which are hidden in the mobile & tablet versions
+  socialTitle.classList.remove('visually-hidden')
+}
+
 function checkDeviceWidth() {
   // if screen is wider than 768px: enable tablet version features
   if (tabletWidth.matches) {
-    // TODO: remove console.logs after script is finished
-    console.log('tablet version')
-
-    // change position of the .stats_legend element
-    smallTextTop.classList.remove('visually-hidden')
-    smallTextBottom.classList.add('visually-hidden')
-
-    // add "tablet elements" which are hidden in the mobile version
-    writeReviewButton.classList.remove('visually-hidden')
-    prevReviewButton.classList.remove('visually-hidden')
-    nextReviewButton.classList.remove('visually-hidden')
-    contactsPhoneMobile.classList.remove('visually-hidden')
+    activateTabletFeatures()
   }
   // if screen is wider than 1200px: also enable desktop version features
   if (desktopWidth.matches) {
-    // TODO: remove console.logs after script is finished
-    console.log('desktop version')
-
-    // add "desktop elements" which are hidden in the mobile & tablet versions
-    socialTitle.classList.remove('visually-hidden')
   }
   // if screen is smaller than 768px: enable only mobile version features
   else {
-    // TODO: remove console.logs after script is finished
-    console.log('mobile version')
-
-    // TODO: maybe make the slides change after click on the whole element?
-    // slider toggles; block: advantages;
-    sliderButtonAdvantages1.addEventListener('change', function (evt) {
-      slide[0].classList.remove('visually-hidden')
-      slide[1].classList.add('visually-hidden')
-      slide[2].classList.add('visually-hidden')
-    })
-
-    sliderButtonAdvantages2.addEventListener('change', function (evt) {
-      slide[0].classList.add('visually-hidden')
-      slide[1].classList.remove('visually-hidden')
-      slide[2].classList.add('visually-hidden')
-    })
-
-    sliderButtonAdvantages3.addEventListener('change', function (evt) {
-      slide[0].classList.add('visually-hidden')
-      slide[1].classList.add('visually-hidden')
-      slide[2].classList.remove('visually-hidden')
-    })
-
-    // button "show all news"; block: news;
-    newsButton.addEventListener('click', function (evt) {
-      // if the news is colapsed(default): expand them
-      if (newsToggleStatus == 0) {
-        evt.preventDefault()
-        // show 3 more news articles and underlines (pseudo-elements) under them
-        for (let i = 2; (i < newsItems.length) & (i != 5); i++) {
-          newsItems[i].classList.remove('visually-hidden')
-          // i - 1 because by default we show 2 news and 1 underline between them
-          newsItems[i - 1].classList.add('news__item--with-underline')
-        }
-        // rename the button
-        newsButton.textContent = 'Скрыть'
-        // change the toggle-status variable
-        newsToggleStatus = 1
-      } else {
-        // if newsToggleStatus != 0 (news are expanded: colapse them)
-        evt.preventDefault()
-
-        // colapse 3 news articles & underlines
-        for (let i = 2; (i < newsItems.length) & (i != 5); i++) {
-          newsItems[i].classList.add('visually-hidden')
-          newsItems[i - 1].classList.remove('news__item--with-underline')
-        }
-        newsButton.textContent = 'Показать все'
-        // scroll the screen to the position with the "show all news" button at the top
-        newsButton.scrollIntoView()
-        // change the toggle-status variable
-        newsToggleStatus = 0
-      }
-    })
-
-    // slider toggles; block: reviews;
-    sliderButtonReviews1.addEventListener('change', function (evt) {
-      slide[3].classList.remove('visually-hidden')
-      slide[4].classList.add('visually-hidden')
-      slide[5].classList.add('visually-hidden')
-    })
-
-    sliderButtonReviews2.addEventListener('change', function (evt) {
-      slide[3].classList.add('visually-hidden')
-      slide[4].classList.remove('visually-hidden')
-      slide[5].classList.add('visually-hidden')
-    })
-
-    sliderButtonReviews3.addEventListener('change', function (evt) {
-      slide[3].classList.add('visually-hidden')
-      slide[4].classList.add('visually-hidden')
-      slide[5].classList.remove('visually-hidden')
-    })
+    activateMobileFeatures()
   }
 }
 
